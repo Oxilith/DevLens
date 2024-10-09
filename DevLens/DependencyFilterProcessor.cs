d3738b2e0eebf724c1364014222b23e2a56208df
Konrad Jagusiak <konrad.jagusiak@gmail.com>
2024-10-10 01:15:19 +0200

    Added missing files to git

---
+++ DevLens/DependencyFilterProcessor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace DevLens;
+
+public class DependencyFilterProcessor(ITelemetryProcessor next) : ITelemetryProcessor
+{
+    public void Process(ITelemetry item)
+    {
+        // Exclude DependencyTelemetry logs
+        if (!(item is DependencyTelemetry))
+        {
+            next.Process(item);
+        }
+    }
+}
