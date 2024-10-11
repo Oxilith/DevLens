d0e354120439a3e2f8465e84300e3d5d5831898f
Konrad Jagusiak <konrad.jagusiak@gmail.com>
2024-10-11 08:48:04 +0200

    added explicit configuration

--- DevLens/Program.cs
+++ DevLens/Program.cs
@@ -10,6 +10,11 @@ using Microsoft.ApplicationInsights.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration.SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables();
+
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
